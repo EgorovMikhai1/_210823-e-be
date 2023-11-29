1ea52900200c3ef6b4bce17a2740ac708c00c5ae
shonmikl <mikhailegorov676@gmail.com>
2023-11-29 19:35:26 +0100

    pic

---
+++ src/main/java/org/example/_2023_11_29/LLExample.java
@@ -0,0 +1,20 @@
+package org.example._2023_11_29;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LLExample {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        List<Integer> list = new LinkedList<>();
+        list.add(55);
+        list.add(525);
+        list.add(553);
+
+        list.add(2, 985);
+        list.clear();
+        System.out.println(System.currentTimeMillis() - start);
+
+    }
+}
