aff79c9522bab5d5ce3b941f66cff809f0ce95d2
shonmikl <mikhailegorov676@gmail.com>
2023-12-20 21:19:00 +0100

    param

---
+++ src/main/java/org/example/_2023_12_20/ex/Adult.java
@@ -0,0 +1,21 @@
+package org.example._2023_12_20.ex;
+
+public class Adult extends Participant{
+    public Adult(String name, int age) {
+        super(name, age);
+    }
+}
+
+class Teenager extends Participant {
+
+    public Teenager(String name, int age) {
+        super(name, age);
+    }
+}
+
+class Pupil extends Participant {
+
+    public Pupil(String name, int age) {
+        super(name, age);
+    }
+}
