12287d17faf0da3b19a38acb88f063f67ee1786b
shonmikl <mikhailegorov676@gmail.com>
2024-01-24 21:13:48 +0100

    IO_4

--- src/main/java/org/example/_2024_01_24/taski/Book.java
+++ src/main/java/org/example/_2024_01_24/taski/Book.java
@@ -16,4 +16,45 @@ class Book implements Serializable {
         this.year = year;
         this.price = price;
     }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- src/main/java/org/example/_2024_01_24/taski/Main.java
+++ src/main/java/org/example/_2024_01_24/taski/Main.java
@@ -1,15 +1,53 @@
 package org.example._2024_01_24.taski;
 
-/**
- * Прочитайте данные из файла data.txt используя FileInputStream и Scanner.
- * Создайте объекты класса Book для каждой строки.
- * Используйте Stream API для преобразования строк в объекты.
- * Сериализуйте список книг в файл с использованием ObjectOutputStream.
- */
 public class Main {
+
+    /**
+     * Создайте объекты класса Book для каждой строки.
+     * Используйте Stream API для преобразования строк в объекты.
+     * Сериализуйте список книг в файл с использованием ObjectOutputStream.
+     */
     public static void main(String[] args) throws RuntimeException {
 
-//            List<Book> books = scanner.useDelimiter("\\A").next().lines()
+    }
+}
 
+class Main1 {
+
+    /**
+     * Десериализуйте список книг из файла,
+     * созданного в предыдущем задании, используя ObjectInputStream.
+     * Используйте Stream API для фильтрации книг, например, по автору или году издания.
+     * Результаты сохраните в новый сериализованный файл
+     */
+    public static void main(String[] args) {
+
+    }
+}
+
+class Main2 {
+
+    /**
+     * Отфильтруйте книги с ценой выше определенной суммы. Сериализуйте отфильтрованный список книг в файл.
+     */
+    public static void main(String[] args) {
+
+    }
+}
+
+class Main3 {
+
+    /**
+     Агрегируйте данные, например, подсчитайте среднюю цену книг по каждому автору. Сериализуйте результаты в файл.
+     */
+    public static void main(String[] args) {
+//        try (FileInputStream fis = new FileInputStream("data.txt");
+//             Scanner scanner = new Scanner(fis);
+//             ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("authorAveragePrice.ser"))) {
+//
+//            Map<String, Double> authorAveragePrice = scanner.useDelimiter("\\A").next().lines()
+//                    .skip(1)
+//                    .map(line -> line.split(", "))
+//
     }
 }
