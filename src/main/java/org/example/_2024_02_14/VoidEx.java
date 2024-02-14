1f93a9dbd332bd4053cd4e473e80037269d66020
shonmikl <mikhailegorov676@gmail.com>
2024-02-14 20:17:36 +0100

    unit1

---
+++ src/main/java/org/example/_2024_02_14/VoidEx.java
@@ -0,0 +1,24 @@
+package main.java.org.example._2024_02_14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VoidEx {
+    private List<String> list;
+
+    public VoidEx() {
+        this.list = new ArrayList<>();
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void addToList(String s) {
+        list.add(s);
+    }
+
+    public void modifyList() {
+        list.replaceAll(el -> el + "*");
+    }
+}
---
+++ src/main/java/org/example/_2024_02_14/VoidExTest.java
@@ -0,0 +1,36 @@
+package main.java.org.example._2024_02_14;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VoidExTest {
+    private static final VoidEx VOID_EX = new VoidEx();
+    private static final String S_1 = "1";
+    private static final String S_2 = "2";
+    private static final String S_3 = "3";
+
+    @BeforeEach
+    public void setUp() {
+        VOID_EX.getList().clear();
+        VOID_EX.addToList(S_1);
+        VOID_EX.addToList(S_2);
+        VOID_EX.addToList(S_3);
+    }
+
+    @Test
+    void addToListTest() {
+        Assertions.assertEquals(3, VOID_EX.getList().size());
+    }
+
+    @Test
+    void modifyListTest() {
+        VOID_EX.modifyList();
+        Assertions.assertEquals(Arrays.asList("1*", "2*", "3*"), VOID_EX.getList());
+    }
+}
