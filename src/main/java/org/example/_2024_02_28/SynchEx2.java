bcc61f477f358d5a4b6a7022dfb6cf082823799d
shonmikl <mikhailegorov676@gmail.com>
2024-02-28 21:17:41 +0100

    MT

--- src/main/java/org/example/_2024_02_28/SynchEx2.java
+++ src/main/java/org/example/_2024_02_28/SynchEx2.java
@@ -1,23 +1,30 @@
 package main.java.org.example._2024_02_28;
 
 public class SynchEx2 {
+    private static final Object LOCK = new Object();
 
     void m1() throws InterruptedException {
-        System.out.println("METHOD_1 START");
-        Thread.sleep(2222);
-        System.out.println("METHOD_1 END");
+        synchronized (LOCK) {
+            System.out.println("METHOD_1 START");
+            Thread.sleep(2222);
+            System.out.println("METHOD_1 END");
+        }
     }
 
     void m2() throws InterruptedException {
-        System.out.println("METHOD_2 START");
-        Thread.sleep(3333);
-        System.out.println("METHOD_2 END");
+        synchronized (LOCK) {
+            System.out.println("METHOD_2 START");
+            Thread.sleep(3333);
+            System.out.println("METHOD_2 END");
+        }
     }
 
     void m3() throws InterruptedException {
-        System.out.println("METHOD_3 START");
-        Thread.sleep(1234);
-        System.out.println("METHOD_3 END");
+        synchronized (LOCK) {
+            System.out.println("METHOD_3 START");
+            Thread.sleep(1234);
+            System.out.println("METHOD_3 END");
+        }
     }
 
     public static void main(String[] args) {
